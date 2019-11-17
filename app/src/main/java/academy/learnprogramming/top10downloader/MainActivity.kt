bd374e93f118529be4163afc0b619314a14ed3a0
Fabrício Magalhães <fabricio.lvm@gmail.com>
2019-11-17 12:06:09 -0300

    Custom Adapter Code

--- app/src/main/java/academy/learnprogramming/top10downloader/MainActivity.kt
+++ app/src/main/java/academy/learnprogramming/top10downloader/MainActivity.kt
@@ -68,8 +68,10 @@ class MainActivity : AppCompatActivity() {
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
 
-                val arrayAdaptor = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parseApplications.applications)
-                propListView.adapter = arrayAdaptor
+//                val arrayAdaptor = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parseApplications.applications)
+//                propListView.adapter = arrayAdaptor
+                val feedAdapter = FeedAdapter(propContext, R.layout.list_record, parseApplications.applications)
+                propListView.adapter = feedAdapter
             }
 
             override fun doInBackground(vararg params: String?): String {
